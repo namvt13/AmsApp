664243eaa9adc604f7119f5c1b748decada77d69
Phung Van Tan <anthonyttan13@gmail.com>
2020-08-22 22:32:26 +0700

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'eslint-comments/no-unlimited-disable': "off",
+    'no-alert': "off",
+    'react-native/no-inline-styles': "off",
+  }
 };
