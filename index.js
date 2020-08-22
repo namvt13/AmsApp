c57e3bf8395d7bce99d939713692fee5089c2b49
Phung Van Tan <anthonyttan13@gmail.com>
2020-08-22 22:38:03 +0700

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './App/Config/ReactotronConfig';
+api.on is not a functionimport './App/Config/ReactotronConfig';
 import {AppRegistry} from 'react-native';
 import App from './App/Containers/App';
 
